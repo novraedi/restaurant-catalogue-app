7e936c1f81caedcbef47ec8ae40f3510803f61bf
novraedi <novraepe@gmail.com>
2024-01-01 18:33:25 +0700

    style: update class names to follow BEM convention

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,13 +5,13 @@ import Data from'../public/data/DATA.json'
 console.log('Hello Coders! :)');
 
 // hamburger
-const hamburger = document.querySelector('.hamburger')
+const hamburger = document.querySelector('.header__hamburger')
 const navList = document.querySelector('.nav-list')
-const navItem = document.querySelector('.nav-item a')
+const navItem = document.querySelector('.nav-list__item a')
 
 hamburger.addEventListener('click', ()=>{
     hamburger.classList.toggle('clicked')
-    navList.classList.toggle('active')
+    navList.classList.toggle('.nav-list--active')
     navItem.focus()
 })
 
@@ -20,9 +20,9 @@ const header = document.querySelector('header')
 
 document.addEventListener('scroll', () => {
     if (window.scrollY > 0) {
-      header.classList.add('scrolled');
+      header.classList.add('header--scrolled');
     } else {
-      header.classList.remove('scrolled');
+      header.classList.remove('header--scrolled');
     }
 });
 
@@ -35,36 +35,37 @@ restaurantData.forEach((data)=>{
   restaurantItem.classList.add('restaurant-item')
 
   const restaurantImg = document.createElement('img')
-  restaurantImg.classList.add('restaurant-img')
+  restaurantImg.classList.add('restaurant-item__img')
   restaurantImg.src = data.pictureId
   restaurantImg.alt = `Gambar restaurant bernama ${data.name}`
   restaurantImg.setAttribute('tabindex', '0')
 
-  const restaurantText = document.createElement('div')
-  restaurantText.classList.add('restaurant-text')
+  const restaurantBody = document.createElement('div')
+  restaurantBody.classList.add('restaurant-item__body')
   
   const restaurantRating = document.createElement('h3')
-  restaurantRating.classList.add('restaurant-rating')
+  restaurantRating.classList.add('restaurant-item__rating')
   restaurantRating.innerText = `Rating: ${data.rating}`
   restaurantRating.setAttribute('tabindex', '0')
 
   const restaurantName = document.createElement('h3')
-  restaurantName.classList.add('restaurant-name')
+  restaurantName.classList.add('restaurant-item__name')
   restaurantName.innerText = data.name
   restaurantName.setAttribute('tabindex', '0')
 
   const restaurantDescription = document.createElement('p')
+  restaurantDescription.classList.add('restaurant-item__description')
   restaurantDescription.innerText = data.description
   restaurantDescription.setAttribute('tabindex', '0')
   
   const restaurantCity = document.createElement('p')
-  restaurantCity.classList.add('restaurant-city')
+  restaurantCity.classList.add('restaurant-item__city')
   restaurantCity.innerText = `Kota. ${data.city}`
   restaurantCity.setAttribute('tabindex', '0')
 
-  restaurantText.append(restaurantCity, restaurantRating,restaurantName,restaurantDescription)
+  restaurantBody.append(restaurantCity, restaurantRating,restaurantName,restaurantDescription)
 
-  restaurantItem.append(restaurantImg, restaurantText)
+  restaurantItem.append(restaurantImg, restaurantBody)
   
   restaurantList.appendChild(restaurantItem)
 })
